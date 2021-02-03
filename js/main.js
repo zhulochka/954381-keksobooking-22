a533a33144b12fc8d301c2d1a22a292cbd8bddb5
Irina Lazareva <zhulochka@gmail.com>
2021-02-03 18:32:09 +0300

    добавляет 2 функции

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomIntInclusive = (min, max) => {
+  if (min >= 0 && min <= max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+getRandomIntInclusive();
+
+const getRandomArbitraryInclusive = (min, max, digits) => {
+  if (min >= 0 && min <= max && digits >= 0) {
+    return (Math.random() * (max - min + 1) + min).toFixed(digits);
+  }
+}
+getRandomArbitraryInclusive();
+
